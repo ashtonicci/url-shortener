fe2c8a82eeb01450ebb765095ae2abd010df9bc0
ashtonicci <samuelashton4@gmail.com>
2020-06-17 15:39:28 +0100

    Making short_url a unique field

---
+++ database/migrations/2020_06_16_163601_create_urls_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUrlsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('urls', function (Blueprint $table) {
+            $table->id();
+            $table->text("full_url");
+            $table->string("short_url")->unique();
+            $table->text("description")->nullable();
+            $table->integer("times_used");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('urls');
+    }
+}
